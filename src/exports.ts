b7ea66e4d81cab5e53fc7f1df10b49855be6886a
albrrtyy <75955302+greenbobux@users.noreply.github.com>
2022-06-10 00:13:24 -0700

    fix jsx typings

---
+++ src/exports.ts
@@ -0,0 +1,6 @@
+export { createElement } from "./createElement";
+export { render } from "./renderer";
+export { createDependency } from "./hooks/createDependency";
+export { createEffect } from "./hooks/createEffect";
+export { createSignal } from "./hooks/createSignal";
+export { externalSignal } from "./hooks/externalSignal";
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
+/// <reference path="./jsx.d.ts" />
 /*
  * Yet Another Roblox User Interface Library */
 // this library was written with spaghetti code
 //
-
-export * from "./renderer";
-export * from "./createElement";
-export * from "./hooks/createEffect";
-export * from "./hooks/createSignal";
-export * from "./hooks/createDependency";
-export * from "./hooks/externalSignal";
+import * as Yaruil from "./exports";
+export default Yaruil;
